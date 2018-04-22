ef98b2628f4213d12088217ebfade807ad7046aa
Octavian <octavianrotari@gmail.com>
2018-04-22 20:46:01 +0100

    testing insurance relations

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,7 +1,5 @@
 class Insurance < ApplicationRecord
-  belongs_to :user
-  has_many :insurance_receipts, dependent: :destroy
-
-  has_and_belongs_to_many :companies
+  belongs_to :vendor
   has_and_belongs_to_many :vehicles
+  has_many :insurance_receipts, dependent: :destroy
 end
--- spec/models/insurance_spec.rb
+++ spec/models/insurance_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Insurance, type: :model do
-  it { should belong_to(:user) }
-  it { should have_many(:insurance_receipts) }
-  it { should have_and_belong_to_many(:companies) }
+  it { should belong_to(:vendor) }
   it { should have_and_belong_to_many(:vehicles) }
+  it { should have_many(:insurance_receipts) }
 end
