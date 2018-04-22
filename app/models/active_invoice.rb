13d693999940c2dc11d3a8f4d8a25f1544614655
Octavian <octavianrotari@gmail.com>
2018-04-22 20:54:55 +0100

    testing active invoice relations

--- app/models/active_invoice.rb
+++ app/models/active_invoice.rb
@@ -1,8 +1,8 @@
 class ActiveInvoice < ApplicationRecord
-  belongs_to :user
+  belongs_to :vendor
   has_many :sold_line_items, dependent: :destroy
 
-  has_one :company, through: :companies_invoices
-  has_and_belongs_to_many :payments
+  has_and_belongs_to_many :loads
   has_and_belongs_to_many :vehicles
+  has_and_belongs_to_many :revenues
 end
--- spec/models/active_invoice_spec.rb
+++ spec/models/active_invoice_spec.rb
@@ -1,31 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ActiveInvoice, type: :model do
-  it { should belong_to(:user) }
+  it { should belong_to(:vendor) }
   it { should have_many(:sold_line_items) }
-  it { should have_and_belong_to_many(:payments) }
-
-  describe 'user create a active invoice' do
-    let(:user) { create(:user) }
-    let(:category) { create(:category, user_id: user.id) }
-    let(:company) { create(:company, category_id: category.id) }
-
-    before :each do
-      user
-      category
-      company
-    end
-
-    it 'fails if there is no user id' do
-      active_invoice = build(:active_invoice)
-      active_invoice.save
-      expect(active_invoice.errors.full_messages).to eq(["User must exist"])
-    end
-
-    it 'fails if there is no company_id' do
-      active_invoice = build(:active_invoice, user_id: user.id)
-      active_invoice.save
-      expect(active_invoice.errors.full_messages).to eq([])
-    end
-  end
+  it { should have_and_belong_to_many(:loads) }
+  it { should have_and_belong_to_many(:revenues) }
 end
