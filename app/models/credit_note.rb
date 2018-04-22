530d4aaedeae9421aa252556fd120847dd73ecec
Octavian <octavianrotari@gmail.com>
2018-04-22 21:00:46 +0100

    testing credit note relations

--- app/models/credit_note.rb
+++ app/models/credit_note.rb
@@ -1,2 +1,4 @@
 class CreditNote < ApplicationRecord
+  belongs_to :vendor
+  has_and_belongs_to_many :revenues
 end
--- spec/models/credit_note_spec.rb
+++ spec/models/credit_note_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe CreditNote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:vendor) }
+  it { should have_and_belong_to_many(:revenues) }
 end
