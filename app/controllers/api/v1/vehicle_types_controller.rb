c797ee22e72a0c60e143df1c4f8f0792b3b8fa34
Octavian <octavianrotari@gmail.com>
2018-07-03 19:49:11 +0100

    creates vehicle types

--- app/controllers/api/v1/vehicle_types_controller.rb
+++ app/controllers/api/v1/vehicle_types_controller.rb
@@ -4,5 +4,23 @@ module Api::V1
       @vehicle_types = current_user.vehicle_types
       json_response(@vehicle_types)
     end
+
+    def create
+      begin
+        vehicle_type = current_user.vehicle_types.new(vehicle_params)
+        if vehicle_type.save
+          head :created, location: v1_vehicle_types_url(vehicle_type)
+        else
+          head :unprocessable_entity
+        end
+      rescue => e
+        json_response({message: e}, :unprocessable_entity)
+      end
+    end
+
+    private
+    def vehicle_params
+      params.require(:vehicle_type).permit(:desc)
+    end
   end
 end
--- spec/requests/vehicle_types_controller_spec.rb
+++ spec/requests/vehicle_types_controller_spec.rb
@@ -19,4 +19,38 @@ RSpec.describe 'Vehicle types Api', type: :request do
       expect(vehicle_types['desc']).to eq('MyString')
     end
   end
+
+  describe 'POST /v1/vehicle_types' do
+    let(:user) { create(:user) }
+    let(:auth_headers) { user.create_new_auth_token }
+    let(:valid_params) do
+      {
+        vehicle_type: {
+          desc: 'autocarro'
+        }
+      }
+    end
+    let(:invalid_params) do
+      {
+        vehicle_type: {}
+      }
+    end
+
+    it 'create a vehicle type' do
+      expect {
+        post "/v1/vehicle_types",
+        headers: auth_headers,
+        params: valid_params
+      }.to change(VehicleType, :count).by(+1)
+      expect(response).to have_http_status :created
+    end
+
+    it 'create a vehicle type error' do
+      post "/v1/vehicle_types",
+      headers: auth_headers,
+      params: invalid_params
+      expect(response).to have_http_status :unprocessable_entity
+      expect(json['message']).to eq('param is missing or the value is empty: vehicle_type')
+    end
+  end
 end
