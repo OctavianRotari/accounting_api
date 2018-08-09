f7b92bab18b545341e9890f02e6f22a972d58fa1
Octavian <octavianrotari@gmail.com>
2018-08-09 19:26:30 +0100

    CRUD other expense

--- app/controllers/api/v1/other_expenses_controller.rb
+++ app/controllers/api/v1/other_expenses_controller.rb
@@ -1,9 +1,49 @@
 module Api::V1
   class OtherExpensesController < ApiController
-
     def index
-      @other_expenses = current_user.other_expenses
-      json_response(@other_expenses)
+      other_expenses = current_user.other_expenses
+      json_response(other_expenses)
+    end
+
+    def create
+      begin
+        other_expense = OtherExpense.new(other_expense_params)
+        other_expense[:user_id] = current_user.id
+        if other_expense.save
+          head :created, location: v1_other_expenses_url(other_expense)
+        else
+          head :unprocessable_entity
+        end
+      rescue => e
+        json_response({message: e}, :unprocessable_entity)
+      end
+    end
+
+    def update
+      other_expense.update(other_expense_params)
+      if(other_expense.save) 
+        head :no_content
+      else
+        head :unprocessable_entity
+      end
+    end
+
+    def destroy
+      other_expense.destroy
+      head :no_content
+    end
+
+    private
+    def other_expense_params
+      params.require(:other_expense).permit(
+        :desc,
+        :total,
+        :date
+      )
+    end
+
+    def other_expense
+      current_user.other_expenses.find_by(id: params[:id])
     end
   end
 end
--- spec/requests/other_expenses_controller_spec.rb
+++ spec/requests/other_expenses_controller_spec.rb
@@ -19,4 +19,72 @@ RSpec.describe 'Other expenses Api', type: :request do
       expect(other_expense['total']).to eq('10.3')
     end
   end
+
+  describe 'POST /v1/other_expenses' do
+    let(:user) { create(:user) }
+    let(:auth_headers) { user.create_new_auth_token }
+    let(:valid_params) do
+      {
+        other_expense: {
+          desc: 'caffe',
+          total: 10.3,
+          date: Date.today(),
+        }
+      }
+    end
+
+    let(:invalid_params) do
+      { other_expense: {} }
+    end
+
+    it 'creates other expense' do
+      expect {
+        post '/v1/other_expenses',
+        headers: auth_headers,
+        params: valid_params
+      }.to change(OtherExpense, :count).by(+1)
+      expect(response).to have_http_status :created
+    end
+
+    it 'creates invoice error' do
+      post '/v1/other_expenses',
+      headers: auth_headers,
+      params: invalid_params 
+      expect(response).to have_http_status :unprocessable_entity
+      expect(json['message']).to eq('param is missing or the value is empty: other_expense')
+    end
+  end
+
+  describe 'PUT /v1/other_expense' do
+    let(:user) { create(:user) }
+    let(:auth_headers) { user.create_new_auth_token }
+    let(:other_expense) { create(:other_expense, user_id: user.id) }
+    let(:valid_params) do
+      {
+        other_expense: {
+          desc: 'caffe',
+          total: 10.3,
+          date: Date.today(),
+        }
+      }
+    end
+
+    it 'updates other_expense' do
+      put "/v1/other_expenses/#{other_expense.id}",
+        headers: auth_headers,
+        params: valid_params
+      expect(response).to have_http_status :no_content
+    end
+  end
+
+  describe 'Delete /v1/other_expense' do
+    let(:user) { create(:user) }
+    let(:auth_headers) { user.create_new_auth_token }
+    let(:other_expense) { create(:other_expense, user_id: user.id) }
+
+    it 'deletes other_expense' do
+      delete "/v1/other_expenses/#{other_expense.id}", headers: auth_headers
+      expect(response).to have_http_status :no_content
+    end
+  end
 end
